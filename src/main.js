3e1a2088cf41030be564980dc3d6b75c475d8a9f
lyz <961829889@qq.com>
2019-11-30 15:31:24 +0800

    添加首页轮播图

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import fastclick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   el: '#app',
